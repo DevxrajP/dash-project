37c69ebb0e1a4a5c26022b59fa265e0fd5eedc8e
DevxrajP <devaraj00726j@gmail.com>
2026-05-22 23:28:45 +0530

    Added sales visualisation Dash app

--- app.py
+++ app.py
@@ -1,26 +1,44 @@
-from dash import Dash, html, dcc, callback, Output, Input
+from dash import Dash, html, dcc
 import plotly.express as px
 import pandas as pd
 
-df = pd.read_csv(
-    'https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv'
+# Read processed CSV file
+df = pd.read_csv("formatted_output.csv")
+
+# Convert date column to datetime
+df["date"] = pd.to_datetime(df["date"])
+
+# Sort by date
+df = df.sort_values("date")
+
+# Group sales by date
+sales_by_date = df.groupby("date")["sales"].sum().reset_index()
+
+# Create line chart
+fig = px.line(
+    sales_by_date,
+    x="date",
+    y="sales",
+    title="Pink Morsel Sales Over Time",
+    labels={
+        "date": "Date",
+        "sales": "Sales"
+    }
 )
 
+# Create Dash app
 app = Dash(__name__)
 
-app.layout = [
-    html.H1(children='Title of Dash App', style={'textAlign': 'center'}),
-    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
-    dcc.Graph(id='graph-content')
-]
+app.layout = html.Div([
+    html.H1(
+        "Soul Foods Sales Visualiser",
+        style={"textAlign": "center"}
+    ),
 
-@callback(
-    Output('graph-content', 'figure'),
-    Input('dropdown-selection', 'value')
-)
-def update_graph(value):
-    dff = df[df.country == value]
-    return px.line(dff, x='year', y='pop')
+    dcc.Graph(
+        figure=fig
+    )
+])
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
